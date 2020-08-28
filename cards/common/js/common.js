75ec57ca11bcbccc45f1fc01b9213d61dff089dc
capaldo <alex@capaldo.io>
2020-08-28 13:30:23 -0400

    add additional text replacement functions

--- cards/common/js/common.js
+++ cards/common/js/common.js
@@ -1,21 +1,31 @@
-changeText = function(text, newText){
+changeTags = function(text, newText){
     var currentText = $('.tags').html();
     $('.tags').html(currentText.replace(text,newText));
 };
 
+changeBasicText = function(text, newText){
+    var currentText = $('.front').html();
+    $('.front').html(currentText.replace(text,newText));
+};
+
 window.setTimeout(function(){
-	changeText(/(\b\s\b)/gm, '<span style="font-size:25px;position:relative;top:2px;border-right:5px solid var(--gray2);margin-right:15px;margin-left:15px;"></span>'),
-	changeText('Basic', '<span id="basic" style="font-size:25px;position:relative;top:2px;">&#xfb37;</span>'), 
-	changeText('Cloze', '<span id="cloze" style="font-size:25px;position:relative;top:2px;">&#xf549;</span>'), 
-	changeText(/MCAT/g, '<span id="mcat" style="font-size:25px;position:relative;top:3px;padding-right:20px;border-right:5px solid var(--gray2);">&#xf0fe;</span>'),
-	changeText(/MISSING/g, '<span id="missing" style="font-size:25px;position:relative;top:2px;margin-right:8px;color:var(--yellow);">&#xf071;</span>'),
-	changeText(/Physiology/, '<span id="physiology" style="font-size:25px;position:relative;top:2px;">&#xe23a;</span>'),
-	changeText(/Physiology::/, ''),
-	changeText(/Histology/, '<span id="histology" style="font-size:25px;position:relative;top:2px;">&#xe222;</span>'),
-	changeText(/Histology::/, ''),
-	changeText(/Biochemistry/, '<span id="biochemistry" style="font-size:25px;position:relative;top:2px;margin-right:8px;">&#xfbdf;</span>'),
-	changeText(/Biochemistry::/, ''),
-	changeText(/\:\:/g, '<span style="font-size:25px;margin-left:10px;margin-right:10px;position:relative;top:3px;">&#x2023;</span>')
+	changeTags(/(\b\s\b)/gm, '<span style="font-size:20px;position:relative;top:2px;border-right:5px solid var(--gray2);margin-right:15px;margin-left:15px;"></span>'),
+	changeTags('Basic', '<span id="basic" style="font-size:20px;position:relative;top:2px;">&#xfb37;</span>'), 
+	changeTags('Cloze', '<span id="cloze" style="font-size:20px;position:relative;top:2px;">&#xf549;</span>'), 
+	changeTags(/MCAT/g, '<span id="mcat" style="font-size:20px;position:relative;top:3px;padding-right:20px;border-right:5px solid var(--gray2);">&#xf0fe;</span>'),
+	changeTags(/MISSING/g, '<span id="missing" style="font-size:20px;position:relative;top:2px;margin-right:8px;color:var(--yellow);">&#xf071;</span>'),
+	changeTags(/Physiology/, '<span id="physiology" style="font-size:20px;position:relative;top:2px;">&#xe23a;</span>'),
+	changeTags(/Physiology::/, ''),
+	changeTags(/Histology/, '<span id="histology" style="font-size:20px;position:relative;top:2px;">&#xe222;</span>'),
+	changeTags(/Histology::/, ''),
+	changeTags(/Biochemistry/, '<span id="biochemistry" style="font-size:20px;position:relative;top:2px;margin-right:8px;">&#xfbdf;</span>'),
+	changeTags(/Biochemistry::/, ''),
+	changeTags(/\:\:/g, '<span style="font-size:20px;margin-left:10px;margin-right:10px;position:relative;top:3px;">&#x2023;</span>'),
+	changeBasicText(/(\*)/gm, '<span style="font-size:40px;font-weight:bold;color:var(--purple);">*</span>')
+}, 0)
+
+
+window.setTimeout(function(){
 }, 0)
 
 var config = {
